dc9190adede53b98b2d001625740e1fb6821a91c
Jason Prickett <jpricket@microsoft.com>
2018-01-18 18:51:09 -0500

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TabbyDriver
 This nodeJS app prompts the user for inputs and then creates a Tabby connection in the specified VSTS account.
+
+## To Install
+    >cd TabbyDriver
+    >npm install
+
+## To Run
+    >node main.js --url https://me.vsts.me --project MyProject --pat eh74vmg5rswjk2xajuj3ll7zv6buha2xd7lz2f5n425gd7qd34lq --repo xplatalm/cuckoo --branch master --installationId 75537
+
+    Getting vsts info:
+       collectionId = 2c31a8a4-3ce2-4230-954d-29653ac881d9
+       projectId = adc3d884-75ff-48ed-9c9f-e1f126d5be30
+    Creating connection:
+       Connection created.
---
+++ main.js
@@ -0,0 +1,96 @@
+var prompt = require('prompt');
+var optimist = require('optimist');
+var Client = require('node-rest-client').Client;
+
+var inputs = {
+    url: 'https://me.vsts.me',
+    pat: '',
+    collectionId: '',
+    projectId: '',
+    project: 'MyProject',
+    repoName: 'xplatalm/cuckoo',
+    branch: 'master',
+    installationId: '75537',
+    yamlPath: '.vsts/microsoft.yml'
+}
+
+// Get any command line args to override prompts
+prompt.override = optimist.argv;
+
+function run(inputs) {
+    prompt.start();
+    var schema = {
+        properties: {
+          url: { required: true, description: "VSTS account URL", default: inputs.url },
+          project: { required: true, description: "Project name", default: inputs.project},
+          pat: { required: true, description: "PAT token", default: inputs.pat, hidden: true},
+          repo: { required: true, description: "GitHub repo name", default: inputs.repoName },
+          branch: { required: true, description: "Repo branch", default: inputs.branch },
+          installationId: { required: true, description: "GitHub app installation id", default: inputs.installationId },
+        }
+      };
+    prompt.get(schema, function (err, result) {
+        inputs.url = result.url;
+        inputs.pat = result.pat;
+        inputs.project = result.project;
+        inputs.repoName = result.repo;
+        inputs.branch = result.branch;
+        inputs.installationId = result.installationId;
+
+        getVstsInfo(inputs);
+    });
+}
+
+function getPatAuthorizationHeader(pat) {
+    const auth = 'Basic ' + new Buffer('pat:' + pat).toString('base64');
+    return auth;
+}
+
+function getVstsInfo(inputs) {
+    const client = new Client();
+    const args = {
+        headers: { 
+            'accept': 'application/json',
+            'Authorization': getPatAuthorizationHeader(inputs.pat)
+        }
+    };
+
+    console.log('Getting vsts info:')
+    client.get(inputs.url + '/_git/' + inputs.project + '/vsts/info', args, function (data, response) {
+        console.log(`   collectionId = ${data.collection.id}`);
+        console.log(`   projectId = ${data.repository.project.id}`);
+        inputs.collectionId = data.collection.id;
+        inputs.projectId = data.repository.project.id;
+        createConnection(inputs);
+    });        
+}
+
+function createConnection(inputs) {
+    const client = new Client();
+    const args = {
+        headers: { 
+            'content-type': 'application/json',
+            'Authorization': getPatAuthorizationHeader(inputs.pat)
+        },
+        data: `{
+                "accountId": "${inputs.collectionId}",
+                "teamProjectId": "${inputs.projectId}",
+                "repositoryId": "${inputs.repoName}",
+                "repositoryName": "${inputs.repoName}",
+                "targetBranch": "${inputs.branch}",
+                "yamlPath": "${inputs.yamlPath}",
+                "providerData": "${inputs.installationId}"
+               }`        
+    };
+
+    console.log('Creating connection:')
+    client.post(inputs.url + '/_apis/Pipelines/Connections?provider=github&api-version=4.1-preview', args, function (data, response) {
+        if (Buffer.isBuffer(data)) {
+            console.log(new Buffer(data).toString('ascii'));
+        } else if (data && data.message) {
+            console.log(data.message);
+        }
+    });        
+}
+ 
+run(inputs);
